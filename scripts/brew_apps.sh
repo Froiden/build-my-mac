a7b68c73bd66bfc3c98c11ce61bda2302139124a
Ajay <ajay@froiden.com>
2020-08-25 13:40:47 +0530

    Feat: first commit

---
+++ scripts/brew_apps.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+## MAC APPS
+# shellcheck disable=SC2039
+echo -e "\e[96m Installing Slack  \e[39m"
+brew cask install slack
+
+echo -e "\e[96m Installing spectacle  \e[39m"
+brew cask install spectacle
+
+echo -e "\e[96m Installing Google Chrome  \e[39m"
+brew cask install google-chrome
+
+echo -e "\e[96m Installing Postman  \e[39m"
+brew cask install postman
+
+echo -e "\e[96m Installing Iterm  \e[39m"
+brew cask install iterm2
+
+echo -e "\e[96m Installing FileZilla  \e[39m"
+brew cask install filezilla
+
+echo -e "\e[96m Installing Grammarly  \e[39m"
+brew cask install grammarly
+
+echo -e "\e[96m Installing spectacle  \e[39m"
+brew cask install spectacle
+
+echo -e "\e[96m Installing alfred  \e[39m"
+brew cask install alfred
+
+echo -e "\e[96m Installing querious  \e[39m"
+brew cask install querious
+
+echo -e "\e[96m Installing evernote  \e[39m"
+brew cask install evernote
+
+
+echo -e "\e[96m Installing Zoom  \e[39m"
+brew cask install zoom
+
+echo -e "\e[96m Installing spark  \e[39m"
+brew cask install spark
+
+
+echo -e "\e[96m Installing teamviewer  \e[39m"
+brew cask install teamviewer
+
+echo -e "\e[96m Installing vlc  \e[39m"
+brew cask install vlc
+
+echo -e "\e[96m Installing hyperswitch  \e[39m"
+brew cask install hyperswitch
+
+echo -e "\e[96m Installing sublime-text  \e[39m"
+brew cask install sublime-text
+
+echo -e "\e[96m Installing google-drive  \e[39m"
+brew cask install google-drive
+
+echo -e "\e[96m Installing AnyDesk  \e[39m"
+brew cask install anydesk
+
+echo -e "\e[96m Installing Upwork  \e[39m"
+brew cask install upwork
+
+echo -e "\e[96m Installing Upwork  \e[39m"
+brew cask install phpstorm
+
+echo -e "\e[96m Installing Free Download Manager  \e[39m"
+brew cask install free-download-manager
+
+
+## CLI TOOLS
+
+echo -e "\e[96m Installing fortune  \e[39m"
+brew install fortune
+
+echo -e "\e[96m Installing cowsay  \e[39m"
+brew install cowsay
+
+echo -e "\e[96m Installing git  \e[39m"
+brew install git
+
+echo -e "\e[96m Installing vcprompt  \e[39m"
+brew install vcprompt
+
+echo -e "\e[96m Installing awscli  \e[39m"
+brew install awscli
+
+echo -e "\e[96m Installing wget  \e[39m"
+brew install wget
+
+echo -e "\e[96m Installing curl  \e[39m"
+brew install curl
