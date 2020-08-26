08041bb8cf327bd4e35b198276fac248ffb07390
Ajay Kumar Choudhary <ajay@froiden.com>
2020-08-26 11:28:10 +0530

    Update mas_apps.sh

--- scripts/mas_apps.sh
+++ scripts/mas_apps.sh
@@ -20,3 +20,5 @@ mas install 985584499    # Tawk.to (1.5)
 mas install 425264550    # Blackmagic Disk Speed Test (3.2)
 mas install 1355679052   # Dropover - Easier Drag & Drop (3.6.6)
 mas install 1176895641   # Spark - Mail
+mas install 1298486723   # Filezilla
+
