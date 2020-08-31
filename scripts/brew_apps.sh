54f6db63c3411f1e6a3d2299456935dd0d836029
Ajay Kumar Choudhary <ajay@froiden.com>
2020-08-31 13:31:05 +0530

    Update brew_apps.sh

--- scripts/brew_apps.sh
+++ scripts/brew_apps.sh
@@ -2,37 +2,5 @@
 
 ## MAC APPS
 # shellcheck disable=SC2039
-brew cask install slack
-brew cask install spectacle
-brew cask install google-chrome
-brew cask install postman
-brew cask install iterm2
-brew cask install filezilla
-brew cask install grammarly
-brew cask install spectacle
-brew cask install alfred
-brew cask install querious
-brew cask install evernote
-brew cask install zoom
-brew cask install teamviewer
-brew cask install vlc
-brew cask install hyperswitch
-brew cask install sublime-text
-brew cask install anydesk
-brew cask install upwork
-brew cask install phpstorm
-brew cask install free-download-manager
-brew cask install skype
-brew cask install zeplin
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
-## CLI TOOLS
-brew install fortune
-brew install cowsay
-brew install git
-brew install vcprompt
-brew install awscli
-brew install wget
-brew install curl
-brew install composer
-brew install mas
